f0c929b710ac3629f9b35bcdfb4c05675efdee16
Aleksandra <bogdanova466@yandex.ru>
2026-09-25 16:32:07 +0300

    добавление 3 тестов в интегриенты

--- tests/test_ingredient.py
+++ tests/test_ingredient.py
@@ -1,9 +1,5 @@
 import pytest
 from praktikum.ingredient import Ingredient
-from praktikum.ingredient_types import (
-    INGREDIENT_TYPE_SAUCE,
-    INGREDIENT_TYPE_FILLING,
-)
 from tests.constants import ALL_INGREDIENTS
 
 
@@ -31,12 +27,4 @@ class TestIngredient:
     )
     def test_get_type_returns_type(self, name, price, ingredient_type):
         ingredient = Ingredient(ingredient_type, name, price)
-        assert ingredient.get_type() == ingredient_type
-
-    @pytest.mark.parametrize(
-        "ingredient_type",
-        [INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING],
-    )
-    def test_type_matches_constructor(self, ingredient_type):
-        ingredient = Ingredient(ingredient_type, "test", 100)
         assert ingredient.get_type() == ingredient_type
